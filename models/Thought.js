93221c3d3f6c1178fb8b94e9328626a8a4dd734d
FavioA <134189396+FavioA@users.noreply.github.com>
2023-12-08 23:05:41 -0800

    Added models and routes directories with files and logic.

---
+++ models/Thought.js
@@ -0,0 +1,31 @@
+
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [ReactionSchema],
+});
+
+// Virtual for reactionCount
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
